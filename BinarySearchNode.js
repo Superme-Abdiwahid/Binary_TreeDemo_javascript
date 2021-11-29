0fef6750131b734cde4729addc79c970ea2f2aa5
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-11-28 19:35:13 -0800

    Update binary tree demo

---
+++ BinarySearchNode.js
@@ -0,0 +1,24 @@
+'use strict'
+
+
+class {
+    /* Method constructs  a bianry tree with leaf nodes. Null to start out with */ 
+    constructor(data, left, right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+        this.overallRoot = null;
+
+    }
+    BinarySearchNode(data){
+        data = data;
+        this(data, null, null);
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
